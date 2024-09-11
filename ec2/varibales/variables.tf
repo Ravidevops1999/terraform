a1fb18b763fa9c346110f94311f84ffd18bb4f4b
Raviteja <rajaraviteja.marampudi1999@gmail.com>
2024-09-11 22:41:11 +0530

    variables

---
+++ ec2/varibales/variables.tf
@@ -0,0 +1,52 @@
+variable "ami_id" {
+    type        = string
+    default     = "ami_09c813fb71547fc4f"
+    description = " This is the AMI ID of Devops"
+}
+
+variable "Instance_type"{
+    type    = string
+    default = "t3.micro"
+}
+
+variable "tags"{
+    type    = map
+    default = {
+        Name = "backend"
+        Project = "Expense"
+        Component = "backend"
+        Environment = "Dev"
+        Terraform = "true"
+    }
+}
+
+variable "sg_name"{
+    default = "allow_ssshhh"
+
+}
+
+variable "sg_description"{
+    default = "Allow port number:22 for ssh access"
+}
+
+variable "from_port"{
+    default = 22
+    type    = number
+    
+}
+
+variable "to_port"{
+    default = 22
+    type    = number
+    
+}
+
+variable "protocol"{
+    default = "tcp"
+    
+}
+
+variable "ingress_eidr"{
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
